59f57cfc91e5f2fede4bfb3842778724537a0a3b
leyla li <leylali@qq.com>
2020-03-18 13:46:30 -0700

    grab data within vue to method

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 new Vue({
     el: '#app',
     data: {
-        title: 'Learning Vue'
+        title: 'Learning Vue',
+        name: 'Leyla'
     },
     methods: {
-        greet(name){
-            return `Hello ${name}!`;
+        greet(){
+            return `Go ${this.name}!`;
         }
     }
 })
