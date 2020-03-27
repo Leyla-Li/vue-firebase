431a588fea3d7302afed16b71e27ed5f1ff96387
leyla li <leylali@qq.com>
2020-03-27 13:42:18 -0700

    add navbar to the root component

--- CLI/src/App.vue
+++ CLI/src/App.vue
@@ -1,12 +1,20 @@
 <template>
   <div id="app">
     <h1>{{ title }}</h1> 
+    <Navbar />
+
   </div>
 </template>
 
 <script>
+import Navbar from './Navbar'
+
 export default {
   name: 'app',
+  components: {
+    // Navbar: Navbar or use ES6 JUST
+    Navbar
+  },
   data () {
     return {
       title: 'My first Vue app',
--- CLI/src/Navbar.vue
+++ CLI/src/Navbar.vue
@@ -13,8 +13,24 @@ export default {
     name: 'Navbar',
     data () {
         return {
-            
+
         }
     }
 }
-</script>>
+</script>
+
+<style>
+nav{
+    text-align: center;
+}
+
+nav ul{
+    padding: 0;
+}
+
+nav li{
+    display: inline-block;
+    list-style-type: none;
+    margin: 0;
+}
+</style>
