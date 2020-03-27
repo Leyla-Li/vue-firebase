52a5f678e002376cccc11daee571e7dea9127cae
leyla li <leylali@qq.com>
2020-03-27 14:59:00 -0700

    add beforecreated and created hook

--- CLI/src/App.vue
+++ CLI/src/App.vue
@@ -1,17 +1,25 @@
 <template>
   <div id="app">
-    
+    <h1>{{ title }}</h1>
+    <Navbar /> 
+    <Blogs />   
   </div>
 </template>
 
 <script>
+import Navbar from './Navbar'
+import Blogs from './Blogs'
 
 export default {
   name: 'app',
+  components: {
+    Navbar,
+    Blogs
+  },
   
   data () {
     return {
-      
+      title: 'My first vue blog'
     }
   },
   methods: {
---
+++ CLI/src/Blogs.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class='blogs'>
+    <h2>{{ blogTitle }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Blogs',
+
+  data(){
+    return{
+      blogTitle: 'Leyla\'s blog'
+    }
+  },
+// hooks
+  beforeCreate(){
+    alert('before create hook')
+  },
+  created(){
+    alert('created hook')
+  }
+}
+</script>
