37b92feab2527cc8daf340b970f83b7d866014fa
leyla li <leylali@qq.com>
2020-03-27 15:45:43 -0700

    moved all the cli related files to cli folder

--- CLI/src/App.vue
+++ CLI/src/App.vue
@@ -1,25 +1,17 @@
 <template>
   <div id="app">
-    <h1>{{ title }}</h1>
-    <Navbar /> 
-    <Blogs />   
+    
   </div>
 </template>
 
 <script>
-import Navbar from './Navbar'
-import Blogs from './Blogs'
 
 export default {
   name: 'app',
-  components: {
-    Navbar,
-    Blogs
-  },
   
   data () {
     return {
-      title: 'My first vue blog'
+      
     }
   },
   methods: {
---
+++ CLI/src/CLI/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <h1>{{ title }}</h1>
+    <Navbar /> 
+    <Blogs />   
+  </div>
+</template>
+
+<script>
+import Navbar from './Navbar'
+import Blogs from './Blogs'
+
+export default {
+  name: 'app',
+  components: {
+    Navbar,
+    Blogs
+  },
+  
+  data () {
+    return {
+      title: 'My first vue blog'
+    }
+  },
+  methods: {
+    
+  }
+}
+</script>
+
+<style>
+
+</style>
