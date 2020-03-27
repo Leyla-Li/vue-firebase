27ec068e46db1f9081618962d065cf21105f405d
leyla li <leylali@qq.com>
2020-03-27 15:02:27 -0700

    create a button and method to change title and add before update hook

--- CLI/src/Blogs.vue
+++ CLI/src/Blogs.vue
@@ -1,6 +1,7 @@
 <template>
   <div class='blogs'>
     <h2>{{ blogTitle }}</h2>
+    <button @click='changeTitle'>Change Title</button>
   </div>
 </template>
 
@@ -13,12 +14,21 @@ export default {
       blogTitle: 'Leyla\'s blog'
     }
   },
+
+  methods: {
+    changeTitle(){
+      this.blogTitle = 'Sushi\'s blog'
+    }
+  },
 // hooks
   beforeCreate(){
     alert('before create hook')
   },
   created(){
     alert('created hook')
+  },
+  beforeUpdate(){
+    alert('before update hook');
   }
 }
 </script>
