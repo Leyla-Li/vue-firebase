37fc898d222ede7765ef093d44d3fca4dcc16d79
leyla li <leylali@qq.com>
2020-04-01 14:33:30 -0700

    add watch and a method to make sure when params change, the component update

--- routing/src/components/ViewProfile.vue
+++ routing/src/components/ViewProfile.vue
@@ -11,6 +11,17 @@ export default {
         return {
           userId: this.$route.params.user_id
         }
+    },
+
+    methods: {
+      updateId(){
+        this.userId = this.$route.params.user_id
+      }
+    },
+
+    //without it when the route params change, if we go from one profile to another, the component will not change, now whenever the route change, it fires the method and change userId
+    watch: {
+      $route: 'updateId'
     }
 }
 </script>
