78eeb5fc9f22817a81afe8cad90695e5ddd142f8
leyla li <leylali@qq.com>
2020-04-02 13:27:19 -0700

    create a navbar component

---
+++ ninja-smoothies/src/components/Navbar.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="navbar">
+        <nav class="nav-extended indito darken-2">
+            <div class="nav-content">
+                <router-link to="">
+                    <span class="nav-title">Ninja Smoothies</span>
+                </router-link>
+                <a href="" class="btn-floating btn-large halfway-fab pink">
+                    <router-link to=""></router-link>
+                </a>
+            </div>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Navbar',
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
