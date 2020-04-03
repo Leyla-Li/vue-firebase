0b45453e023440c8ae8817284773d3ad182efe70
leyla li <leylali@qq.com>
2020-04-03 13:25:04 -0700

    when click delte, it deltes from database

--- ninja-smoothies/src/components/index.vue
+++ ninja-smoothies/src/components/index.vue
@@ -26,9 +26,10 @@ export default {
   },
   methods: {
     deleteSmoothie(id){
-      this.smoothies = this.smoothies.filter(smoothie => {
-        return smoothie.id !== id
-      })
+      db.collection('smoothies').doc(id).delete()
+        .then(()=>{
+          
+        })
     }
   },
 
