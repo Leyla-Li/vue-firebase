26344c782a22270269cd18fe7c19e83ace9edef8
leyla li <leylali@qq.com>
2020-04-06 13:47:40 -0700

    edit method linked to update database

--- ninja-smoothies/src/components/EditSmoothie.vue
+++ ninja-smoothies/src/components/EditSmoothie.vue
@@ -30,6 +30,7 @@
 
 <script>
 import db from '@/firebase/init'
+import slugify from 'slugify'
 
 export default {
   name:'EditSmoothie',
@@ -43,7 +44,27 @@ export default {
 
   methods: {
     EditSmoothie(){
-      console.log(this.smoothie.title, this.smoothie.ingredients)
+      if (this.smoothie.title){
+        this.feedback = null
+        this.slug = slugify(this.smoothie.title, {
+          replacement: '-',
+          remove: /[$*_+~.()'"!\-:@]/g,
+          lower: true
+        })
+        db.collection('smoothies').doc(this.smoothie.id).update({
+          title: this.smoothie.title,
+          ingredients: this.smoothie.ingredients,
+          slug: this.smoothie.slug
+        })
+          .then(() => {
+            this.$router.push({ name: 'Index' })
+          })
+          .catch(err => {
+            console.log(err)
+          })
+      }else{
+        this.feedback = 'You must enter a smoothie title'
+      }
     },
 
     addIngredient(){
