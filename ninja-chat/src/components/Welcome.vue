378d1bab41755781f00fa136b7accd5c84b87de5
leyla li <leylali@qq.com>
2020-04-07 13:06:54 -0700

    add params name to route when redirect to chat

--- ninja-chat/src/components/Welcome.vue
+++ ninja-chat/src/components/Welcome.vue
@@ -28,7 +28,7 @@ export default {
   methods: {
     enterChat(){
       if(this.name){
-        this.$router.push({name: 'Chat', })
+        this.$router.push({name: 'Chat', params: this.name})
       } else{
         this.feedback = 'Please enter a name to join the chat'
       }
