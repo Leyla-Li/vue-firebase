95a0036e541e148de6d82097433caebcea93be0d
leyla li <leylali@qq.com>
2020-04-06 17:51:48 -0700

    create a form and name input

--- ninja-chat/src/components/Welcome.vue
+++ ninja-chat/src/components/Welcome.vue
@@ -1,6 +1,15 @@
 <template>
-  <div class="welcome">
-    
+  <div class="welcome container">
+    <div class="card">
+      <div class="card-content center-align">
+        <h2 class="teal-text">Welcome to the chat</h2>
+        <form @submit.prevet="enterChat">
+          <label for="name">Enter Your Name:</label>
+          <input type="text" name="name" v-model="name" />
+          <button class="btn teal">Enter Chat</button>
+        </form>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -8,7 +17,8 @@
 export default {
   name: 'Welcome',
   data () {
-    return {
+    return {  
+      name: null
     }
   }
 }
