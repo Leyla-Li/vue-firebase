2f4706ded1f695d47656e93e25843ba924b407b8
leyla li <leylali@qq.com>
2020-04-06 17:47:50 -0700

    deleted default content

---
+++ ninja-chat/src/components/Welcome.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="welcome">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Welcome',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+
+</style>
