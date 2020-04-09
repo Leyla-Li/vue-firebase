acec96369b3cf93eb28ce0b8a45475ba0d01fef3
leyla li <leylali@qq.com>
2020-04-09 11:25:38 -0700

    import google map when mounted

--- geo-ninja/src/components/home/GMap.vue
+++ geo-ninja/src/components/home/GMap.vue
@@ -9,22 +9,37 @@ export default {
   name:'GMap',
   data(){
     return {
-
+      lat: 53,
+      lng: -2
     }
   },
 
   methods: {
     renderMap(){    
-      const map = new google.maps.Map()
+      const map = new google.maps.Map(document.getElementById('map'),{
+        center:{lat: this.lat, lng: this.lng},
+        zoom: 6,
+        maxZoom: 15,
+        minZoom: 3,
+        streetViewControl: false
+      })
     }
   },
 
   mounted(){
-    renderMap();
+    this.renderMap();
   },
 }
 </script>
 
 <style>
-
+.google-map{
+  width: 100%;
+  height: 100%;
+  margin: 0 auto;
+  background: #fff;
+  position: absolute;
+  top: 0;
+  left: 0;
+}
 </style>
