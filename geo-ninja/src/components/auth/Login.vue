bd07b684125b37d347342ecbf5044c9e40abd6cc
leyla li <leylali@qq.com>
2020-04-09 14:53:57 -0700

    redirect to gmap page when logged in

--- geo-ninja/src/components/auth/Login.vue
+++ geo-ninja/src/components/auth/Login.vue
@@ -37,7 +37,7 @@ export default {
         
         firebase.auth().signInWithEmailAndPassword(this.email, this.password)
           .then(cred => {
-            console.log(cred.user)
+            this.$router.push({name: 'GMap'})
           })
           .catch(err => {
             this.feedback = err.message
