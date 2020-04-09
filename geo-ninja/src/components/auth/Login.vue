575000ed3610379513638ecb06a7fa87f3355289
leyla li <leylali@qq.com>
2020-04-09 14:38:30 -0700

    create form for login page

---
+++ geo-ninja/src/components/auth/Login.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="login container">
+    <form class="card-panel">
+      <h2 class="center deep-purple-text">Login</h2>
+      <div class="field">
+        <label for="email">E-mail:</label>
+        <input type="email" name="email" v-model="email">
+      </div>
+      <div class="field">
+        <label for="password">Password:</label>
+        <input type="password" name="password" v-model="password">
+      </div>
+      <p class="red-text center" v-if="feedback">{{ feedback }}</p>
+      <div class="field center">
+        <button class="btn deep-purple">Login</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data(){
+    return {
+      email: null,
+      password: null,
+      feedback: null
+    }
+  },
+
+  methods: {
+    
+  }
+}
+</script>
+
+<style>
+
+</style>
