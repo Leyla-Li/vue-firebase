a1c6bf5686e0d76d5d6338adf15d41d4c6f58257
leyla li <leylali@qq.com>
2020-04-09 13:06:37 -0700

    create input for email, password and alias

--- geo-ninja/src/components/auth/Signup.vue
+++ geo-ninja/src/components/auth/Signup.vue
@@ -6,6 +6,14 @@
         <label for="email">E-mail:</label>
         <input type="email" name="email">
       </div>
+      <div class="field">
+        <label for="password">Password:</label>
+        <input type="password" name="password">
+      </div>
+      <div class="field">
+        <label for="alias">Alias:</label>
+        <input type="alias" name="alias">
+      </div>
     </form>
   </div>
 </template>
