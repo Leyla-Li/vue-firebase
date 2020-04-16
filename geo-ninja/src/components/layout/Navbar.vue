d177407cd6d01f67ed2297d4692822623edf8f02
leyla li <leylali@qq.com>
2020-04-16 16:03:05 -0700

    add style to logo

--- geo-ninja/src/components/layout/Navbar.vue
+++ geo-ninja/src/components/layout/Navbar.vue
@@ -2,7 +2,7 @@
   <div class="navbar">
     <nav class="deep-purple darken-1">
       <div class="container">
-        <router-link :to="{name: 'GMap'}">GeoNinjas</router-link>
+        <router-link :to="{name: 'GMap'}" class="brand-logo left">GeoNinjas</router-link>
         <ul class="right">
           <li v-if="!user">
             <router-link :to="{name: 'Signup'}">Signup</router-link>
