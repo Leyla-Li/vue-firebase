4a695ee492ee779d21454dca2e0a3b3cec0c462c
leyla li <leylali@qq.com>
2020-04-17 14:30:11 -0700

    create a form for comments

--- geo-ninja/src/components/profile/ViewProfile.vue
+++ geo-ninja/src/components/profile/ViewProfile.vue
@@ -2,6 +2,15 @@
   <div class="view-profile container">
     <div v-if="profile" class="card">
       <h2 class="deep-purple-text center">{{profile.alias}}'s Wall</h2>
+      <ul class="comments collection">
+        <li>Comment</li>
+      </ul>
+      <form @submit.prevent="addComment">
+        <div class="field">
+          <label for="comment">Add a comment</label>
+          <input type="text" name="comment" v-model="newComment" />
+        </div>
+      </form>
     </div>
   </div>
 </template>
